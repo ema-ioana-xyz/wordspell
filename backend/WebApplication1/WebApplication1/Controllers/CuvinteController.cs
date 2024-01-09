776c95192d9504eab2c6682b08e9943c7590056e
Țiț Ioana Emilia <EmaTit@protonmail.com>
2024-01-09 19:49:47 +0200

    Add backend to repo

---
+++ backend/WebApplication1/WebApplication1/Controllers/CuvinteController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/cuvinte")]
+    public class CuvinteController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCuvinte()
+        {
+            try
+            {
+                string json = System.IO.File.ReadAllText("cuvinte.json");
+                var cuvinte = JsonConvert.DeserializeObject<List<Cuvinte>>(json);
+                return Ok(cuvinte);
+            }
+            catch (FileNotFoundException)
+            {
+                return NotFound("Fișierul nu a fost găsit.");
+            }
+            catch (JsonException)
+            {
+                return StatusCode(500, "Eroare la deserializare JSON.");
+            }
+        }
+    }
+}
---
+++ backend/WebApplication1/WebApplication1/Models/Cuvinte.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Cuvinte
+    {
+        public int Id { get; set; }
+        public string Cuvant { get; set; }
+        public string UrlSunet { get; set; }
+        public string UrlImagine { get; set; }
+    }
+}
